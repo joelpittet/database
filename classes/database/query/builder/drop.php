01869e4ce440966c42c056d1ac4245b28339cd84
Freshinc <oliver.morgan@freshinc.co.uk>
2009-11-09 02:32:59 +0800

    Added standard classes.

---
+++ classes/database/query/builder/drop.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Database query builder for DROP statements.
+ *
+ * @package    Database
+ * @author     Kohana Team
+ * @copyright  (c) 2008-2009 Kohana Team
+ * @license    http://kohanaphp.com/license.html
+ */
+class Kohana_Database_Query_Builder_Drop extends Database_Query_Builder { }
