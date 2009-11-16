7da997d6becedd2c2cb4b0bf2491088613296fd6
Freshinc <oliver.morgan@freshinc.co.uk>
2009-11-16 17:20:17 +0800

    Fixed prefixes in class.

--- classes/database/table/column/string.php
+++ classes/database/table/column/string.php
@@ -1,5 +1,5 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Kohana_Database_Table_Column_String extends Database_Table_Column_String {
+class Database_Table_Column_String extends Kohana_Database_Table_Column_String {
 	
 }
